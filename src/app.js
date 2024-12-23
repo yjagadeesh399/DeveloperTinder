5a6693d2fc7d7da0370d3b607c3d598803af9206
Bharathnimm <bharathroy666@gmail.com>
2024-12-23 13:42:14 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express"); 
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("Hello your are listening to server")
+});
+app.use("/hello",(req,res)=>{
+    res.send("Hello hello hello hello .......!!!!!")
+});
+
+app.listen(3000, ()=>{console.log("We are using the SERVER")});
