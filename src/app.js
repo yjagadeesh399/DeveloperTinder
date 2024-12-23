7879546cf754775c55953ddff4afe240d4bc8e55
Bharathnimm <bharathroy666@gmail.com>
2024-12-23 16:08:22 +0530

    Exploring Get, Put, Delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello your are listening to server")
-});
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello hello .......!!!!!")
-});
+
+
+// app.use("/test",(req,res)=>{
+//     res.send("Hello your are listening to server")
+// });
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello hello hello hello .......!!!!!")
+// });
+
+app.get("/user", (req,res)=>{res.send("Used get method to get data")});
+
+app.post("/user", (req,res)=>{res.send("Used get method to post data")});
+
+app.delete("/user", (req,res)=>{res.send("Used get method to delete data")});
+
+app.use("/",(req,res)=>{res.send("This is route .......!!!!!")});
 
 app.listen(3000, ()=>{console.log("We are using the SERVER")});
 
